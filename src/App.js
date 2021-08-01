67e7146262c437c38bccf1d4617a5a237b050c79
Namdevmane2 <manenamdev2@gmail.com>
2021-08-01 22:58:28 +0530

    list and event

--- src/App.js
+++ src/App.js
@@ -3,11 +3,17 @@ import "./App.css";
 import { useState } from "react";
 
 function App() {
-  let name = "Namdev";
-  let list = ["A", "w", "b", "c", "d", "e"];
+  let list = ["hello", "Hero"];
+
+  const addNewHello = () => {
+    alert("add new Hello logic program here.....! ");
+    list.push("hello");
+    console.log(list);
+  };
   return (
     <div>
-      <h2>Hello world</h2>
+      <h2>Working with events and list </h2>
+      <input type="button" value="Say Hello" onClick={addNewHello} />
       {list.map((item) => {
         return <li> {item} </li>;
       })}
